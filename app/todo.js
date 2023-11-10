fbe771d588ef65a21849a407d33f43c028bf4ce9
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-11-10 13:00:01 +0400

    todo part 1

--- app/todo.js
+++ app/todo.js
@@ -7,9 +7,37 @@ import {
   TextInput,
   FlatList,
   TouchableOpacity,
+  StyleSheet,
 } from "react-native";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "buy coffee",
+      key: 1,
+    },
+    {
+      text: "Problem Solving",
+      key: 2,
+    },
+    {
+      text: "Create TODO APP",
+      key: 3,
+    },
+    {
+      text: "learning react-native",
+      key: 4,
+    },
+  ]);
+
+  const [value, setValue] = useState();
+
+  const pressHandler = (key) => {
+    setTodos((prevState) => {
+      return prevState.filter((e) => e.key != key);
+    });
+  };
+
   return (
     <View>
       <Stack.Screen
@@ -23,9 +51,55 @@ export default function App() {
         }}
       />
 
-      <Text>Todo</Text>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Task Əlavə Edin"
+          style={styles.input}
+          // onKeyPress={(keyPress) => {
+          //   console.log(keyPress);
+          //   if (e.nativeEvent.key == "Enter") {
+          //     // dismissKeyboard();
+          //     dismissKeyboard();
+          //   }
+          // }}
+          multiline={true}
+          autoCapitalize="sentences"
+          autoCorrect={true}
+          keyboardType="default"
+          returnKeyType="done"
+          onChangeText={(value) => {
+            // todos.unshift({
+            //   text: value,
+            //   key: value.length,
+            // });
+          }}
+        />
+
+        <FlatList
+          data={todos}
+          keyExtractor={(item) => item.key}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => pressHandler(item.key)}>
+              <Text style={styles.flatlistElement}>
+                {item.text} {item.key}
+              </Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
     </View>
   );
 }
 
-// const styles = {};
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  input: {
+    borderColor: "teal",
+    borderWidth: 1,
+    padding: 8,
+    margin: 10,
+    width: 320,
+  },
+});
